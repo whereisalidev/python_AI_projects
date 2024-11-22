8ff3504af9f673ba2bbc65e4e3ed65df13d755dd
whereisalidev <786syedalihussain@gmail.com>
2024-11-22 15:54:11 +0500

    updated

---
+++ py.py
@@ -0,0 +1,63 @@
+# # # TASK_01: Write Python Program to Calculate the Length of a String
+# # # Without Using Built-In len() Function.
+# # name = "AhmedAli"
+# # ch = 0
+# # for i in name:
+# #     ch += 1
+# # print(f"Total Characters in {name}: {ch}")
+
+
+
+# # # TASK_02: Write a program that creates a list of 10 random integers.Then 
+# # # create two lists by name odd_list and even_list that have all odd
+# # # and even values of the list respectively.
+
+# # odd_list = []
+# # even_list = []
+# # list = [2,4,3,8,1,9,5,17,11,18,10]
+
+# # for i in list:
+# #     if i % 2 == 0:
+# #         even_list.append(i)
+# #     else:
+# #         odd_list.append(i)
+
+# # print(f"Even List: {even_list}\nOdd List: {odd_list}")
+
+
+
+# #TASK 3: Explore the following functions of list:
+# list = [1,2,3]
+# #1) list.append(x):
+# list.append(4)
+# print(f"apppend: {list}")
+# #2) list.extend(L):
+# a = [1,2,3,4]
+# l = [5,6]
+# a.extend(l)
+# print(f"extend: {a}")
+# #3) list.insert(i, x):
+# x = [0,10]
+# x.insert(1, 5)
+# print(f"insert: {x}")
+# #4) list.remove(x):
+# x.remove(10)
+# print(f"remove: {x}")
+# #5) list.pop():
+# x.pop()
+# print(f"pop: {x}")
+# #6) list.count(x):
+# l1 = [2,3,5,2,7,7,4,7]
+# print(f"count: {l1.count(7)}")
+# #7) list.sort():
+# l1.sort()
+# print(f"sort: {l1}")
+# #8) list.reverse():
+# l1.reverse()
+# print(f"reverse: {l1}")
+
+
+
+
+
+# take as an input of 
